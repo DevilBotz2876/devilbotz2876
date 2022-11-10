ce99c4fb2c5b6da019751f713d1c172a6fa6c8e5
rohit18-cool <rohitvijayakumar09@gmail.com>
2022-11-09 19:43:09 -0500

    Updated the title

--- python/parsingxml.py
+++ python/parsingxml.py
@@ -1,27 +1,8 @@
 import requests
 import xml.etree.ElementTree as ET
-from bs4 import BeautifulSoup
-
-# Reading data from the xml file
-with open('sponsorship-levels/blogs.2022-11-09.xml', 'r') as f:
-	data = f.read()
-
-# Passing the data of the xml
-# file to the xml parser of
-# beautifulsoup
-data_b = BeautifulSoup(data, 'xml')
-
-# A loop for replacing the value
-# of attribute `test` to WHAT !!
-# The tag is found by the clause
-# `bs_data.find_all('child', {'name':'Frank'})`
-for tag in data_b.find_all('child', {'team':'3D CAD Workshop - How to Model Almost Anything'}):
-	tag['test'] = "WHAT !!"
 
 
-# Output the contents of the
-# modified xml file
-print(bs_data.prettify())
+
 
 def parseXML(xmlfile):
   
@@ -37,28 +18,26 @@ def parseXML(xmlfile):
     # iterate news items
     for item in root.findall('./channel/item'):
   
-        # empty news dictionary
-        # news = {}
+        # getting new title items into dictionary
+        new_item = {}
   
         # iterate child elements of item
         for child in item:
-  
-            # special checking for namespace object content:media
-            if child.title == '':
-                pass
+            
+            # # special checking for namespace object content:media
+            if child.title == 'title':
+                new_item.append(child.title)
             else:
-                pass
+                print('Null')
   
-        # append news dictionary to news items list
-        newsitems.append()
+        
+            
 
   
 
 def main():
-  
-  
     # parse xml file
-    x = parseXML('topnewsfeed.xml')
+    x = parseXML('blogs.2022-09-21.xml')
 if __name__ == "__main__":
   
     # calling main function
